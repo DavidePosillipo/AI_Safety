8b94c752e3394c3e441a55f515d98d4831ae11fb
DavidePosillipo <davide.posillipo@icloud.com>
2018-08-25 14:08:05 +0200

    added save dictionaries with losses

--- second_implementation/main.py
+++ second_implementation/main.py
@@ -5,6 +5,8 @@ from models import Generator, Discriminator, Inverter
 from training import Trainer
 from training_inverter import Trainer_inverter
 
+import numpy as np
+
 data_loader, _ = get_mnist_dataloaders(batch_size=64)
 img_size = (32, 32, 1)
 
@@ -24,18 +26,22 @@ D_optimizer = optim.Adam(discriminator.parameters(), lr=lr, betas=betas)
 I_optimizer = optim.Adam(discriminator.parameters(), lr=lr, betas=betas)
 
 # Train model
-epochs = 50
+epochs = 1
 trainer = Trainer(generator, discriminator, G_optimizer, D_optimizer,
                   use_cuda=torch.cuda.is_available())
 trainer.train(data_loader, epochs, save_training_gif=True)
 
 name = 'mnist_model'
-# torch.save(trainer.G.state_dict(), './gen_' + name + '.pt')
-# torch.save(trainer.D.state_dict(), './dis_' + name + '.pt')
-generator.load_state_dict(torch.load("gen_mnist_model.pt"))
-discriminator.load_state_dict(torch.load("dis_mnist_model.pt"))
+torch.save(trainer.G.state_dict(), './gen_' + name + '.pt')
+torch.save(trainer.D.state_dict(), './dis_' + name + '.pt')
+
+np.save("gd_losses.npy", trainer.losses)
+# generator.load_state_dict(torch.load("gen_mnist_model.pt"))
+# discriminator.load_state_dict(torch.load("dis_mnist_model.pt"))
 
 trainer_inverter = Trainer_inverter(inverter, generator, I_optimizer, use_cuda=torch.cuda.is_available())
 trainer_inverter.train(data_loader, epochs)
 
 torch.save(trainer.I.state_dict(), './inv_' + name + '.pt')
+
+np.save("i_losses", trainer_inverter.losses)
