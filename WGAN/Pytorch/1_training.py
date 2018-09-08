fixed pytorch normalization mistake

WGAN/Pytorch/1_training.py
@@ -1,7 +1,7 @@
 import torch
 import torch.optim as optim
 
-from classes.dataloaders import get_mnist_dataloaders, get_lsun_dataloader
+from classes.dataloaders import get_mnist_dataloaders
 from classes.models_wgan import Generator, Discriminator, Inverter
 from classes.training_wgan_inverter import Trainer
 
@@ -10,9 +10,9 @@ import numpy as np
 data_loader, _ = get_mnist_dataloaders(batch_size=80)
 img_size = (32, 32, 1)
 
-generator = Generator(img_size=img_size, latent_dim=64, dim=128)
+generator = Generator(img_size=img_size, latent_dim=64, dim=64)
 discriminator = Discriminator(img_size=img_size, dim=64)
-inverter = Inverter(img_size=img_size, latent_dim=64, dim=128)
+inverter = Inverter(img_size=img_size, latent_dim=64, dim=64)
 
 print(generator)
 print(discriminator)
@@ -36,9 +36,9 @@ trainer = Trainer(generator,
     use_cuda=torch.cuda.is_available())
 trainer.train(data_loader, epochs, save_training_gif=True)
 
-name = 'mnist_model_128'
+name = 'mnist_model_64'
 torch.save(trainer.G.state_dict(), '.models/gen_' + name + '.pt')
 torch.save(trainer.D.state_dict(), '.models/dis_' + name + '.pt')
 torch.save(trainer.I.state_dict(), '.models/inv_' + name + '.pt')
 
-np.save("gdi_losses_128.npy", trainer.losses)
+np.save("gdi_losses_64.npy", trainer.losses)

WGAN/Pytorch/2_distribution_delta_z.py
@@ -11,8 +11,9 @@ from torchvision import datasets
 import torchvision.transforms as transforms
 from torchvision.utils import save_image
 
-from second_implementation.models import Generator, Inverter, Discriminator
-from searching_algorithms import iterative_search, recursive_search
+from classes.models_wgan import Generator, Inverter, Discriminator
+from classes.searching_algorithms import iterative_search, recursive_search
+from classes.dataloaders import get_mnist_dataloaders
 
 from sklearn.metrics import accuracy_score
 
@@ -21,25 +22,18 @@ from sklearn.metrics import accuracy_score
 
 img_size = (32, 32, 1)
 
-generator = Generator(img_size=img_size, latent_dim=64, dim=128)
-discriminator = Discriminator(img_size=img_size, dim=128)
-inverter = Inverter(img_size=img_size, latent_dim=64, dim=128)
+generator = Generator(img_size=img_size, latent_dim=64, dim=64)
+discriminator = Discriminator(img_size=img_size, dim=64)
+inverter = Inverter(img_size=img_size, latent_dim=64, dim=64)
 
-generator.load_state_dict(torch.load("./models/gen_mnist_model_128.pt"))
-inverter.load_state_dict(torch.load("./models/inv_mnist_model_128.pt"))
+generator.load_state_dict(torch.load("./models/gen_mnist_model_64.pt"))
+inverter.load_state_dict(torch.load("./models/inv_mnist_model_64.pt"))
 
 generator.cuda()
 inverter.cuda()
 
 # Training data
-dataloader = torch.utils.data.DataLoader(
-    datasets.MNIST('../data/mnist', train=True, download=True,
-                   transform=transforms.Compose([
-                       transforms.Resize(32),
-                       transforms.ToTensor(),
-                       transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-                   ])),
-    batch_size=1, shuffle=True)
+dataloader, dataloader_test = get_mnist_dataloaders(batch_size=1)
 
 data = list(enumerate(dataloader))
 
@@ -58,16 +52,6 @@ y_2 = np.array(y).reshape(len(dataloader), )
 clf = RandomForestClassifier(max_depth = 10, random_state = 0)
 
 clf.fit(X_2, y_2)
-
-
-dataloader_test = torch.utils.data.DataLoader(
-    datasets.MNIST('../data/mnist', train=False, download=True,
-                   transform=transforms.Compose([
-                       transforms.Resize(32),
-                       transforms.ToTensor(),
-                       transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-                   ])),
-    batch_size=1, shuffle=True)
 
 data_test = list(enumerate(dataloader_test))
 

WGAN/Pytorch/3_chicken.py
@@ -14,8 +14,9 @@ from torchvision import datasets
 import torchvision.transforms as transforms
 from torchvision.utils import save_image
 
-from classes.models import Generator, Inverter, Discriminator
+from classes.models_wgan import Generator, Inverter, Discriminator
 from classes.searching_algorithms import iterative_search, recursive_search
+from classes.dataloaders import get_mnist_dataloaders
 
 from sklearn.metrics import accuracy_score
 
@@ -24,25 +25,18 @@ from sklearn.metrics import accuracy_score
 
 img_size = (32, 32, 1)
 
-generator = Generator(img_size=img_size, latent_dim=64, dim=128)
-discriminator = Discriminator(img_size=img_size, dim=128)
-inverter = Inverter(img_size=img_size, latent_dim=64, dim=128)
+generator = Generator(img_size=img_size, latent_dim=64, dim=64)
+discriminator = Discriminator(img_size=img_size, dim=64)
+inverter = Inverter(img_size=img_size, latent_dim=64, dim=64)
 
-generator.load_state_dict(torch.load("./models/gen_mnist_model_128.pt"))
-inverter.load_state_dict(torch.load("./models/inv_mnist_model_128.pt"))
+generator.load_state_dict(torch.load("./models/gen_mnist_model_64.pt"))
+inverter.load_state_dict(torch.load("./models/inv_mnist_model_64.pt"))
 
 generator.cuda()
 inverter.cuda()
 
 # Training data
-dataloader = torch.utils.data.DataLoader(
-    datasets.MNIST('../data/mnist', train=True, download=True,
-                   transform=transforms.Compose([
-                       transforms.Resize(32),
-                       transforms.ToTensor(),
-                       transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-                   ])),
-    batch_size=1, shuffle=True)
+dataloader, dataloader_test = get_mnist_dataloaders(batch_size=80)
 
 data = list(enumerate(dataloader))
 
@@ -61,16 +55,6 @@ y_2 = np.array(y).reshape(len(dataloader), )
 clf = RandomForestClassifier(max_depth = 10, random_state = 0)
 
 clf.fit(X_2, y_2)
-
-
-dataloader_test = torch.utils.data.DataLoader(
-    datasets.MNIST('../data/mnist', train=False, download=True,
-                   transform=transforms.Compose([
-                       transforms.Resize(32),
-                       transforms.ToTensor(),
-                       transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-                   ])),
-    batch_size=1, shuffle=True)
 
 data_test = list(enumerate(dataloader_test))
 
@@ -95,10 +79,12 @@ def rf_classifier(x):
 
 ### Chicken example
 ck = io.imread('chicken.jpg', as_gray=True)
-ck = resize(ck, (28, 28), anti_aliasing = True)
+ck = resize(ck, (32, 32), anti_aliasing = True)
 ck = np.float32(ck)
+# rescaling to white over black
+ck = 1-ck
 
-ck_data = ck.reshape(1, 28*28)
+ck_data = ck.reshape(1, 32*32)
 
 # RF prediction
 y_hat_rf = rf_classifier(ck)

WGAN/Pytorch/classes/searching_algorithms.py
@@ -144,11 +144,11 @@ def recursive_search(gen_fn, inv_fn, cla_fn, x, y, y_t=None, z=None,
         z_tilde = z + delta_z       # z tilde
         x_tilde = gen_fn(torch.from_numpy(z_tilde.astype(np.float32)).cuda())   # x tilde
         y_tilde = cla_fn(x_tilde.cpu().detach().numpy())   # y tilde
-        print("y_t", y_t)
-        print("y", y)
-        print("y_tilde", y_tilde)
-        print("cla_fn(x)", cla_fn(x.cpu().detach().numpy()))
-        print("cla_fn(gen(inv(x)))", cla_fn(gen_fn(inv_fn(x)).cpu().detach().numpy()))
+        # print("y_t", y_t)
+        # print("y", y)
+        # print("y_tilde", y_tilde)
+        # print("cla_fn(x)", cla_fn(x.cpu().detach().numpy()))
+        # print("cla_fn(gen(inv(x)))", cla_fn(gen_fn(inv_fn(x)).cpu().detach().numpy()))
         if y_t is None:
             indices_adv = np.where(y_tilde != y)[0]
         else:
@@ -207,7 +207,7 @@ def recursive_search(gen_fn, inv_fn, cla_fn, x, y, y_t=None, z=None,
             indices_adv = np.where(y_tilde != y)[0]
         else:
             indices_adv = np.where(y_tilde == y_t)[0]
-        print("len indices_adv", len(indices_adv))
+        #print("len indices_adv", len(indices_adv))
         if len(indices_adv) == 0:
             counter += 1
             printout()