530075cbe7b95158f3f0982d15aca9f9804c851f
DavidePosillipo <davide.posillipo@icloud.com>
2018-09-09 00:35:10 +0200

    modified again loss lenet

--- WGAN/Pytorch/LeNet.py
+++ WGAN/Pytorch/LeNet.py
@@ -30,7 +30,7 @@ def train(args, model, device, train_loader, optimizer, epoch):
         data, target = data.to(device), target.to(device)
         optimizer.zero_grad()
         output = model(data)
-        loss = nn.NLLLoss(output, target)
+        loss = F.nll_loss(output, target)
         loss.backward()
         optimizer.step()
         if batch_idx % args.log_interval == 0:
