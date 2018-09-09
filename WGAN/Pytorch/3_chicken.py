d1e026137e474d83965473e2791bed71010ee2e5
DavidePosillipo <davide.posillipo@icloud.com>
2018-09-09 11:43:22 +0200

    modified pytorch chicken

--- WGAN/Pytorch/3_chicken.py
+++ WGAN/Pytorch/3_chicken.py
@@ -102,7 +102,7 @@ ck_probabilities_rf = clf.predict_proba(ck_data)
 print("estimated probabilities for the chicken (random forest)", ck_probabilities_rf)
 
 # RF prediction
-ck_probabilities_nn = nn_classifier(torch.Tensor(ck))
+ck_probabilities_nn = nn_classifier(torch.Tensor(ck).view(1, 1, 32, 32).cuda())
 print("estimated probabilities for the chicken (LeNet)", ck_probabilities_nn)
 _, y_hat_nn = torch.max(ck_probabilities_nn.data, 1)
 print("estimate for the chicken (LeNet)", y_hat_nn)
@@ -110,12 +110,12 @@ print("estimate for the chicken (LeNet)", y_hat_nn)
 # Delta_z for the chicken
 searcher = recursive_search
 
-adversary_ck_rf = recursive_search(gen_fn, inv_fn, cla_fn_rf, ck_data, y_hat_rf,
+adversary_ck_rf = recursive_search(generator, inverter, cla_fn_rf, ck_data, y_hat_rf,
                    nsamples=5000, step=0.01, verbose=False)
 
 print("delta_z for the chicken (random forest):", adversary_ck_rf["delta_z"])
 
-adversary_ck_nn = recursive_search(gen_fn, inv_fn, cla_fn_nn, torch.Tensor(ck), y_hat_nn,
+adversary_ck_nn = recursive_search(generator, inverter, cla_fn_nn, torch.Tensor(ck).view(1, 1, 32, 32).cuda(), y_hat_nn,
                    nsamples=5000, step=0.01, verbose=False)
 
 print("delta_z for the chicken (LeNet):", adversary_ck_nn["delta_z"])
