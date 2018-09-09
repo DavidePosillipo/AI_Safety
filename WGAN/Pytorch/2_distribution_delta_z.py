2b3b63beb96ec483af8d6fc9476e299acedf6525
DavidePosillipo <davide.posillipo@icloud.com>
2018-09-09 15:34:50 +0200

    undo 2_

--- WGAN/Pytorch/2_distribution_delta_z.py
+++ WGAN/Pytorch/2_distribution_delta_z.py
@@ -24,12 +24,12 @@ from sklearn.metrics import accuracy_score
 
 img_size = (32, 32, 1)
 
-generator = Generator(img_size=img_size, latent_dim=64, dim=128)
-discriminator = Discriminator(img_size=img_size, dim=128)
-inverter = Inverter(img_size=img_size, latent_dim=64, dim=128)
+generator = Generator(img_size=img_size, latent_dim=64, dim=32)
+discriminator = Discriminator(img_size=img_size, dim=32)
+inverter = Inverter(img_size=img_size, latent_dim=64, dim=32)
 
-generator.load_state_dict(torch.load("./models/gen_mnist_model_128.pt"))
-inverter.load_state_dict(torch.load("./models/inv_mnist_model_128.pt"))
+generator.load_state_dict(torch.load("./models/gen_mnist_model_32.pt"))
+inverter.load_state_dict(torch.load("./models/inv_mnist_model_32.pt"))
 
 generator.cuda()
 inverter.cuda()
@@ -41,33 +41,83 @@ le_net.cuda()
 # Training data
 dataloader, dataloader_test = get_mnist_dataloaders(batch_size=1)
 
+data = list(enumerate(dataloader))
+
+X = []
+y = []
+for i in data:
+    X.append(i[1][0].numpy())
+    y.append(i[1][1].numpy())
+
+X_2 = np.array(X)
+X_2 = np.reshape(X_2, (len(dataloader), 1024))
+type(X_2)
+
+y_2 = np.array(y).reshape(len(dataloader), )
+
+clf = RandomForestClassifier(max_depth = 10, random_state = 0)
+
+clf.fit(X_2, y_2)
+
+data_test = list(enumerate(dataloader_test))
+
+X_test = []
+y_test = []
+for i in data_test:
+    X_test.append(i[1][0].numpy())
+    y_test.append(i[1][1].numpy())
+
+X_test = np.array(X_test)
+X_test = np.reshape(X_test, (len(dataloader_test), 1024))
+
+y_test = np.array(y_test).reshape(len(dataloader_test), )
+
+predicted = clf.predict(X_test)
+accuracy = accuracy_score(y_test, predicted)
+
+print("accuracy of the black box classifier", accuracy)
+
+def rf_classifier(x):
+    return clf.predict(np.reshape(x, (-1, 1024)))
+
 def nn_classifier(x):
-    predicted = le_net(x)
-    _, y_hat_nn = torch.max(predicted.data, 1)
-    return y_hat_nn
+    return le_net(x)
 
 searcher = recursive_search
 
 n = len(dataloader_test)
-
 output_delta_z = np.ndarray(n)
-
 for i, data in enumerate(dataloader_test):
     print("test point", i, "over", n)
-    x = data[0].cuda()
+    x = data[0]
     y = data[1]
-    y_pred = nn_classifier(x)
-
-    if y_pred != y.cuda():
+    predictions = nn_classifier(x)
+    _, y_pred = torch.max(predictions.data, 1)
+    if y_pred != y:
         continue
-
     output_delta_z[i] = searcher(generator,
         inverter,
-        nn_classifier,
-        x,
+        rf_classifier,
+        x.cuda(),
         y,
         verbose = False)["delta_z"]
+    print("delta_z of point", i, " :", output_delta_z[i])
 
+n = len(data_test)
+output_delta_z = np.ndarray(n)
+for i in np.arange(n):
+    print("test point", i, "over", n)
+    x = data_test[i][1][0]
+    y = data_test[i][1][1]
+    y_pred = rf_classifier(x)
+    if y_pred != y:
+        continue
+    output_delta_z[i] = searcher(generator,
+        inverter,
+        rf_classifier,
+        x.cuda(),
+        y,
+        verbose = False)["delta_z"]
     print("delta_z of point", i, " :", output_delta_z[i])
 
 np.save("test_deltaz.npy", output_delta_z)
