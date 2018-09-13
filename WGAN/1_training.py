b51aa96fe27b36edfddb5723b6779e6ada5df9c9
Adam Green <adam.green@adgefficiency.com>
2018-09-13 04:08:21 +0200

    added if name main block to WGAN/1_training.py - little bit of formatting

--- WGAN/1_training.py
+++ WGAN/1_training.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch
 import torch.optim as optim
 
@@ -5,40 +6,42 @@ from classes.dataloaders import get_mnist_dataloaders
 from classes.models_wgan import Generator, Discriminator, Inverter
 from classes.training_wgan_inverter import TrainerWGANInv
 
-import numpy as np
+if __name__ == '__main__':
 
-data_loader, _ = get_mnist_dataloaders(batch_size=64)
-img_size = (32, 32, 1)
+    data_loader, _ = get_mnist_dataloaders(batch_size=64)
+    img_size = (32, 32, 1)
 
-generator = Generator(img_size=img_size, latent_dim=64, dim=32)
-discriminator = Discriminator(img_size=img_size, dim=32)
-inverter = Inverter(img_size=img_size, latent_dim=64, dim=32)
+    generator = Generator(img_size=img_size, latent_dim=64, dim=32)
+    discriminator = Discriminator(img_size=img_size, dim=32)
+    inverter = Inverter(img_size=img_size, latent_dim=64, dim=32)
 
-print(generator)
-print(discriminator)
-print(inverter)
+    print(generator)
+    print(discriminator)
+    print(inverter)
 
-# Initialize optimizers
-lr = 1e-4
-betas = (.9, .99)
-G_optimizer = optim.Adam(generator.parameters(), lr=lr, betas=betas)
-D_optimizer = optim.Adam(discriminator.parameters(), lr=lr, betas=betas)
-I_optimizer = optim.Adam(inverter.parameters(), lr=lr, betas=betas)
+    # Initialize optimizers
+    # adamg - could these be part of the classes?
+    lr = 1e-4
+    betas = (.9, .99)
+    G_optimizer = optim.Adam(generator.parameters(), lr=lr, betas=betas)
+    D_optimizer = optim.Adam(discriminator.parameters(), lr=lr, betas=betas)
+    I_optimizer = optim.Adam(inverter.parameters(), lr=lr, betas=betas)
 
-# Train model
-epochs = 200
-trainer = TrainerWGANInv(generator,
-    discriminator,
-    inverter,
-    G_optimizer,
-    D_optimizer,
-    I_optimizer,
-    use_cuda=torch.cuda.is_available())
-trainer.train(data_loader, epochs, save_training_gif=True)
+    # Train model
+    epochs = 200
+    trainer = TrainerWGANInv(generator,
+        discriminator,
+        inverter,
+        G_optimizer,
+        D_optimizer,
+        I_optimizer,
+        use_cuda=torch.cuda.is_available())
 
-name = 'mnist_model_32'
-torch.save(trainer.G.state_dict(), './models/gen_' + name + '.pt')
-torch.save(trainer.D.state_dict(), './models/dis_' + name + '.pt')
-torch.save(trainer.I.state_dict(), './models/inv_' + name + '.pt')
+    trainer.train(data_loader, epochs, save_training_gif=True)
 
-np.save("gdi_losses_32.npy", trainer.losses)
+    name = 'mnist_model_32'
+    torch.save(trainer.G.state_dict(), './models/gen_' + name + '.pt')
+    torch.save(trainer.D.state_dict(), './models/dis_' + name + '.pt')
+    torch.save(trainer.I.state_dict(), './models/inv_' + name + '.pt')
+
+    np.save("gdi_losses_32.npy", trainer.losses)
